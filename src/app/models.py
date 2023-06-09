eeab500c169744a00186de77e8940f7377849dce
Maksim Vikhrov <maxvihr@yandex.ru>
2023-06-09 19:51:30 +0500

    Update: separate auth packadge

--- src/app/models.py
+++ src/app/models.py
@@ -7,29 +7,12 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
-# class Employee(Base):
-#     __tablename__ = 'employees'
-
-#     id = Column(UUID, primary_key=True, index=True, default=uuid.uuid4)
-#     username = Column(String(50), unique=True, nullable=False)
-#     password = Column(String, nullable=False)
-
-#     salary = relationship('Salary', back_populates='employee')
-
-#     def set_password(self, password: str):
-#         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-#         self.password = hashed_password.decode('utf-8')
-    
-#     def verify_password(self, password: str):
-#         return bcrypt.checkpw(password.encode('utf-8'), self.password)
-
-
 class Salary(Base):
-    __tablename__ = 'salaries'
+    __tablename__ = 'salary'
 
     id = Column(UUID, primary_key=True, index=True, default=uuid.uuid4)
     salary = Column(Numeric(precision=10, scale=2))
     increase_date = Column(Date)
 
-    # employee_id = Column(UUID, ForeignKey('employees.id', ondelete='CASCADE'), index=True)
-    # employee = relationship('Employee', back_populates='salary')
+    employee_id = Column(UUID, ForeignKey('user.id', ondelete='CASCADE'), index=True)
+    employee = relationship('Employee', back_populates='salary')
--- src/auth/models.py
+++ src/auth/models.py
@@ -1,7 +1,12 @@
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 
 from database import Base
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
+    
+    username = Column(String(50), nullable=False)
+    salary = relationship('Salary', back_populates='employee')
+
